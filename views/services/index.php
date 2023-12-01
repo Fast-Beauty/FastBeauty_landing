7c30f6e4b330fd7b00a427319350d46cf80fee59
Alejandro Betancourth <luisbetancourth43@gmail.com>
2023-12-01 16:21:04 -0500

    Se finaliza el componente de servicios

--- views/services/index.php
+++ views/services/index.php
@@ -15,13 +15,16 @@
                         <div class="icono icono_review d-flex justify-content-center">
                             <img src="assets/images/icono-reviews-servicio.svg" alt="Icono reseñas">
                         </div>
-                        <p class="fw-semibold fs-2 text-uppercase fuente-secundaria">4.3</p>
+                        <div class="review_date">
+                            <p class="fw-bolder fs-2 text-uppercase fuente-secundaria">4.3</p>
+                            <p class="fw-semibold fs-5 text-uppercase">+ 30 calificaciones</p>
+                        </div>
                     </div>
                     <div class="dato tiempo d-flex align-items-center">
                         <div class="icono icono_tiempo d-flex justify-content-center">
                             <img src="assets/images/icono-tiempo-servicios.svg" alt="Icono tiempo">
                         </div>
-                        <p class="fw-semibold fs-2 fuente-secundaria">40 Mins</p>
+                        <p class="fw-bolder fs-2 fuente-secundaria">40 Mins</p>
                     </div>
                 </div>
                 <div class="reservar-btn d-flex justify-content-start">
