f2e7982c267d575f434edf5fabc6368758049d39
Dar4k <103978453+Dar4k@users.noreply.github.com>
2024-05-30 11:55:17 -0500

    Ajuste #2

--- views/product_details/product_details.php
+++ views/product_details/product_details.php
@@ -3,7 +3,8 @@
     <div class="container">
 
         <!-- Migas de pan -->
-        <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+        <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='currentColor'/%3E%3C/svg%3E&#34;);"
+            aria-label="breadcrumb">
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="?c=Landing&m=landing">Home</a></li>
                 <li class="breadcrumb-item"><a href="?c=Product&m=product">Productos</a></li>
@@ -12,27 +13,96 @@
         </nav>
         <!-- End Migas de pan -->
 
-
-
         <div class="row">
-        <div class="col-md-5">
-                <div class="product-details">
-                    <div class="carousel">
-                            <p>A</p>
-                    </div>
-                </div>
 
+            <div class="col-md-5">
+                <!-- Carrousel product -->
+                <input type="radio" id="image1" name="image" checked>
+                <input type="radio" id="image2" name="image">
+                <input type="radio" id="image3" name="image">
+
+                <div class="containerdetails">
+                    <div class="featured-wrapper">
+                        <ul class="featured-list">
+                            <li>
+                                <figure>
+                                    <img src="./assets/images/producto-jabon.svg" alt="">
+                                </figure>
+                            </li>
+                            <li>
+                                <figure>
+                                    <img src="./assets/images/producto-jabon.svg" alt="">
+                                </figure>
+                            </li>
+                            <li>
+                                <figure>
+                                    <img src="./assets/images/producto-jabon.svg" alt="">
+                                </figure>
+                            </li>
+                        </ul>
+                        <ul class="arrows">
+                            <li>
+                                <label for="image1"></label>
+                            </li>
+                            <li>
+                                <label for="image2"></label>
+                            </li>
+                            <li>
+                                <label for="image3"></label>
+                            </li>
+                        </ul>
+                        <ul class="dots">
+                            <li>
+                                <label for="image1"></label>
+                            </li>
+                            <li>
+                                <label for="image2"></label>
+                            </li>
+                            <li>
+                                <label for="image3"></label>
+                            </li>
+                        </ul>
+                    </div>
+                    <ul class="thumb-list">
+                        <li>
+                            <label for="image1">
+                                <img src="./assets/images/producto-jabon.svg" alt="">
+                                <span class="outer">
+                                    <span class="inner">Caption1</span>
+                                </span>
+                            </label>
+                        </li>
+                        <li>
+                            <label for="image2">
+                                <img src="./assets/images/producto-jabon.svg" alt="">
+                                <span class="outer">
+                                    <span class="inner">Caption2</span>
+                                </span>
+                            </label>
+                        </li>
+                        <li>
+                            <label for="image3">
+                                <img src="./assets/images/producto-jabon.svg" alt="">
+                                <span class="outer">
+                                    <span class="inner">Caption3</span>
+                                </span>
+                            </label>
+                        </li>
+                    </ul>
+                </div>
+                <!-- Carrousel product -->
             </div>
 
             <div class="col-md-5">
                 <div class="product-tex">
                     <h4>Jabón</h4>
                     <p>For Soft, smooth skin</p>
-                    <p> $10.000  </p>
+                    <p> $10.000 </p>
                 </div>
             </div>
 
         </div>
+
     </div>
 
     <div class="flower2">
@@ -41,7 +111,23 @@
 
     <div class="text">
         <h6>Descripción</h6>
+        <p>Elimina el 99.9% sin resecar la piel. La fórmula humectante de Dove está enriquecida con un ingrediente
+            antibacterial, brindándote protección y cuidado para una piel suave, hidratada y protegida.
+            Esta suave fórmula limpiadora contiene 1/4 de crema humectante para darte una piel más suave, sedosa y de
+            apariencia más saludable.
+
+            • Nutrición para la piel seca
+            La barra de belleza Dove Original no te reseca la piel como pueden hacerlo los jabones comunes: contiene ¼
+            de crema humectante, que repone los nutrientes de la piel y la deja nutrida y visiblemente saludable
+
+            • Humectación para a la piel
+            El tradicional ¼ de crema humectante ayuda la piel a mantener su humectación natural, de manera que se
+            siente más suave y tersa incluso antes de que hayas salido de la ducha.
+
+            • Delicada limpieza
+            La barra de belleza Dove no reseca tu piel como los jabones comunes, porque la barra de belleza Dove
+            Original contiene delicados agentes limpiadores que limpian y cuidan la piel.
+            Lo podes usar todos los días tanto en tu cuerpo como en tu rostro.</p>
     </div>
 
 </section>
-
